25b6d9eef93eb012a06c3851168750ce157c8433
purplesun <467364280@qq.com>
2021-11-02 21:19:36 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   console.log('Hello world received a request.')
-  console.log('req body', req)
+  console.log('req body', req.body)
   res.send('Hello, Welcome to CloudBase!')
 })
 
 app.post('/', (req, res) => {
   console.log('Hello world received a request.')
-  console.log('req body', req)
+  console.log('req body', req.body)
   res.send('Hello, Welcome to CloudBase Post!')
 })
 
